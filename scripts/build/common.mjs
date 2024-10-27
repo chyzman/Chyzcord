9f820aadb2047726b09e1f7d6fae002ae1a09d59
chyzman <32403637+chyzman@users.noreply.github.com>
2024-10-27 19:53:57 -0400

    load plugins pls

--- scripts/build/common.mjs
+++ scripts/build/common.mjs
@@ -115,7 +115,7 @@ export const globPlugins = kind => ({
         });
 
         build.onLoad({ filter, namespace: "import-plugins" }, async () => {
-            const pluginDirs = ["plugins/_api", "plugins/_core", "plugins", "userplugins", "equicordplugins"];
+            const pluginDirs = ["plugins/_api", "plugins/_core", "plugins", "userplugins", "equicordplugins", "chyzcordplugins"];
             let code = "";
             let pluginsCode = "\n";
             let metaCode = "\n";
